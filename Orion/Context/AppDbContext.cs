160fa3aa14524069734dd0236718bde7dcceef97
Amr Moawad <amr.moawad@doablelink.com>
2024-03-16 15:04:29 +0200

    Add project files.

---
+++ Orion/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orion.Models;
+
+namespace Orion.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.UseSerialColumns();
+        }
+    }
+}
