a730ea5f0e90459a6ffa8fc459b50eb6f150e082
Amr Moawad <amr.moawad@doablelink.com>
2024-04-03 23:38:04 +0200

    tested frontend

--- Orion/Context/AppDbContext.cs
+++ Orion/Context/AppDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Orion.Models;
 using Orion.Models.Common;
-using Orion.Models.Identity;
 
 namespace Orion.Context
 {
@@ -45,7 +44,6 @@ namespace Orion.Context
         public DbSet<Under12> Under12s { get; set; }
         public DbSet<Vendor> Vendors { get; set; }
         public DbSet<Website> Websites { get; set; }
-        public DbSet<ApplicationUser> Users { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
