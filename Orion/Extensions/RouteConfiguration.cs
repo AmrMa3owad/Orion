9bc7cf0f16d79bbbd941fdd29e03a5a3fe990b42
Amr Moawad <amr.moawad@doablelink.com>
2024-06-01 15:32:15 +0300

    fixed some errors

--- Orion/Extensions/RouteConfiguration.cs
+++ Orion/Extensions/RouteConfiguration.cs
@@ -16,48 +16,42 @@ namespace Orion.Extensions
 
             HttpClient httpClient = new HttpClient
             {
-                BaseAddress = new Uri(
-                configuration["WebApi:Url"])
+                BaseAddress = new Uri(configuration["WebApi:Url"])
             };
             services.AddSingleton(httpClient);
-
         }
+
         public static void ConfigureRouter(this IServiceCollection services)
         {
             services.AddMvc()
                 .AddRazorPagesOptions(options =>
                 {
-                    options.Conventions.AddPageRoute("/EndUser/Home", "Index");
-                    options.Conventions.AddPageRoute("/EndUser/main-products", "main-products");
+                    options.Conventions.AddPageRoute("/EndUser/Home", "Index.html");
+                    options.Conventions.AddPageRoute("/EndUser/main-products", "main-products.html");
                 });
         }
 
-        public static void ConfigureJWT(
-          this IServiceCollection services, IConfiguration configuration)
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
         {
-
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
             })
-              .AddJwtBearer(options =>
-              {
-                  options.IncludeErrorDetails = true;
-
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuerSigningKey = true,
-                      ValidateIssuer = true,
-                      ValidateAudience = true,
-                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
-                      ValidIssuer = configuration["Jwt:Issuer"],
-                      ValidAudience = configuration["Jwt:Audience"]
-                  };
-              });
+            .AddJwtBearer(options =>
+            {
+                options.IncludeErrorDetails = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"]
+                };
+            });
         }
-
     }
 }
--- Orion/Program.cs
+++ Orion/Program.cs
@@ -1,20 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using Orion.Context;
-using Orion.Domain.Models;
 using Orion.Extensions;
 using Orion.Infrastructure.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages().AddRazorPagesOptions(options =>
-{
-    options.Conventions.AddPageRoute("/EndUser/Home", "Index.html");
-    options.Conventions.AddPageRoute("/EndUser/main-products", "main-products.html");
-
-});
-
-builder.Services.AddMvc();
+builder.Services.AddRazorPages();
 builder.Services.ConfigureRouter();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("OrionDB")));
@@ -25,15 +16,8 @@ builder.Services.ConfigureHttpContext();
 builder.Services.ConfigureInfrastructure();
 builder.Services.ConfigureApiVersions();
 builder.Services.ConfigureWebApi(builder.Configuration);
-//builder.Services.ConfigureDbContext(builder.Configuration);
-//builder.Services.ConfigureJWT(builder.Configuration);
-builder.Services.ConfigureMapper();
 builder.Services.ConfigureEmailSender(builder.Configuration);
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orion", Version = "v1" });
-//});
 
 var app = builder.Build();
 
@@ -48,17 +32,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
-app.UseAuthentication();
-
-//app.UseSwagger();
-
-//app.UseSwaggerUI(c =>
-//{
-//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orion v1");
-//    c.RoutePrefix = string.Empty;
-//});
-
 app.MapRazorPages();
 app.MapControllers();
 
